5ab04d33b5eb466a0034f6b204cf7a1c2318f1af
Bill Brinkert <email": null>
2016-01-27 09:45:06 -0800

    Display list of person's badges; fix line 36 of app.js to target class

--- js/app.js
+++ js/app.js
@@ -33,7 +33,8 @@ var showThisPerson = function(this_path){
   }).done(function(response){
     console.log("ajax response: " + response[0].id + " " + response[0].phrase);
     $('.people_list_container').hide();
-    $('show-user').show();
+    $('.show-user').show();
+    showPersonsSlogans(response);
   }).fail(function(response){
     console.log("Ajax fail on showThisPerson.");
   });
